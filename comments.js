75f476b7c51dbedf140f58938defc79f2ccc4e06
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-01-28 01:20:59 +0900

    UZ & 9.html
    Add comment function

---
+++ comments.js
@@ -0,0 +1,34 @@
+function warnEmpty(){
+    alert("뭘 추가해달라는 거죠..?😮")
+}
+
+function dateToString(date){
+    const datestring = date.toISOString();
+    let ko_hour=Number(datestring.substring(11,13))+9;
+    if (ko_hour>24){
+        ko_hour = ko_hour -24;
+    }
+    const datestr2 = datestring.substring(0,10) +" "+ ko_hour+datestring.substring(13,19);
+    return datestr2;
+}
+
+function submitcommit(){
+    const newcomment = document.getElementById("new-comment");
+    const newcommnetval= newcomment.value;
+    const datestr = dateToString(new Date());
+
+    if(newcommnetval){
+        const rowEL = document.createElement('div');
+        rowEL.classList.add('comment-row');
+        rowEL.innerText = datestr +"\n" 
+        rowEL.append(newcommnetval);
+
+        document.getElementById('comment').appendChild(rowEL);
+        newcomment.value = "";
+
+        const cntEL = document.getElementById("comment-count");
+        const cnt = cntEL.innerText;
+        cntEL.innerText = parseInt(cnt) +1;
+
+    } else warnEmpty();
+}
---
+++ comments_appendchild.js
@@ -0,0 +1,33 @@
+function warnEmpty(){
+    alert("뭘 추가해달라는 거죠..?😮")
+}
+
+function dateToString(date){
+    const datestring = date.toISOString();
+    return datestring;
+}
+
+function submitcommit(){
+    const newcomment = document.getElementById("new-comment");
+    const newcommnetval= newcomment.value;
+
+    if(newcommnetval){
+        const dateEL = document.createElement('div');
+        dateEL.classList.add("comment-date");
+        const datestr = dateToString(new Date());
+        dateEL.innerText = datestr;
+
+        const contentEL = document.createElement('div');
+        contentEL.classList.add("comment-content");
+        contentEL.innerText = newcommnetval;
+
+        const rowEL = document.createElement('div');
+        rowEL.classList.add('comment-row');
+        rowEL.appendChild(dateEL);
+        rowEL.appendChild(contentEL);
+        
+        document.getElementById('comment').appendChild(rowEL);
+        newcomment.value = "";
+
+    } else warnEmpty();
+}
