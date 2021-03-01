41479f37a17bd103a21627f12c25ff2b278bfde6
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-03-01 23:27:32 +0900

    navigator
    Add navigator bar
    remodeling home page

--- myscript.js
+++ myscript.js
@@ -73,9 +73,3 @@ function bounce_ani(self){
     setTimeout(function(){self.setAttribute('class',"test")},1000);
 }
 
-let progress = document.getElementById('progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-    let progressHeight=(window.pageYOffset / totalHeight) *100;
-    progress.style.height = progressHeight + "%";
-}
