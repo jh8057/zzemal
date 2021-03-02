a4bed2b35c77946907597ca21881a6d5518bf56a
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-03-03 02:06:51 +0900

    landing
    Add landing page on index.html
    landing.js
    style.css

---
+++ landing.js
@@ -0,0 +1,10 @@
+function drawline(){
+    let circle= document.querySelector('.circle')
+    let text= document.querySelector('.circletext')
+    let text2= document.querySelector('.circletext2')
+
+    circle.style.animation = 'zooming 2s ease forwards'
+    text.style.animation = 'circletext 2s ease forwards'
+    text2.style.animation = 'circletext 4s ease forwards'
+
+}
