d009270f1a37dcb6b5adc6d514d65b558f8d2d32
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-03-05 02:06:16 +0900

    scrollevent
    change bg color

--- scrollevent.js
+++ scrollevent.js
@@ -1,19 +1,38 @@
 let last_known_scroll_position = 0;
 let ticking = false;
+let mybodystyle =  document.querySelector('body').style;
+let myheadstyle =  document.querySelector('h1');
+
+function flash_ani(){
+  myheadstyle.setAttribute('class',"animate__animated animate__flash animate__repeat-1 animate__slow");
+}
 
 function doSomething(scroll_pos) {
-if((scroll_pos)<180){
+if((scroll_pos)<600){
+  mybodystyle.animation = 'bgwhite 1.5s ease forwards'
+  myheadstyle.style.color ="black";
+  //console.log(scroll_pos)
+  } else if (620<(scroll_pos)<1000){
+  mybodystyle.animation = 'bgblack 1.5s ease forwards'
+  myheadstyle.style.color ="white";
+  flash_ani();
+  //console.log(scroll_pos)mybodystyle.animation = 'bgwhite 1.5s ease forwards'
+  }
+if((scroll_pos)>1010){
+  mybodystyle.animation = 'bgwhite 1.5s ease forwards'
+  console.log(scroll_pos)
+}
+if((scroll_pos)<1500){
     document.getElementById("no1").className="NOTSTART";
     document.getElementById("no1").style.opacity = "0";
-} else if ((scroll_pos)>210){
-    //console.log(scroll_pos)
+} else if ((scroll_pos)>1600){
     document.getElementById("no1").className="animate__animated animate__backInLeft";
     document.getElementById("no1").style.opacity = "1";
     }
-if ((scroll_pos)<700){
+if ((scroll_pos)<2100){
     document.getElementById("no2").className="NOTSTART";
     document.getElementById("no2").style.opacity = "0";    
-} else if((scroll_pos)>740) {
+} else if((scroll_pos)>2200) {
     document.getElementById("no2").className="animate__animated animate__fadeInUp";
     document.getElementById("no2").style.opacity = "1";
     countdays();
