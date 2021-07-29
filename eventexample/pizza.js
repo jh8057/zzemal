bd268e8da76351ca1030e1f101c981f059847e2d
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-07-29 11:34:01 +0900

    Pizza
    Make Pizza example

--- eventexample/pizza.js
+++ eventexample/pizza.js
@@ -2,28 +2,41 @@ const form = document.querySelector(".location"),
     dropdown = form.querySelector(".dropdown"),
     dropdowntoggle = dropdown.querySelector(".dropdown-toggle"),
     dropdownmenu = dropdown.querySelector(".dropdown-menu"),
-    dropdownopt = dropdown.querySelector(".dropdown-option"),
+    dropdownopt = dropdown.querySelectorAll(".dropdown-option"),
     nextbtn = form.querySelector(".next-button");
 
-function handleDeleteSelect(e){
+function selected(){
     dropdownmenu.classList.remove("show");
     dropdowntoggle.classList.add("selected");
-    dropdowntoggle.innerText = e.target.innerText
+    nextbtn.removeAttribute("disabled")
 }
 
-function handleAfterSelect(){
-    dropdownmenu.addEventListener("click",handleDeleteSelect)
+function changeInnerText(){
+    // dropdownmenu.addEventListener("click",selected)
+    dropdownopt.forEach((item)=>{
+        item.addEventListener("click",(e)=>{
+            const value = e.currentTarget.textContent.trim();
+            dropdowntoggle.innerText = value;
+            selected()
+        })
+    })
+}
+
+function removeShow(){
+    dropdownmenu.classList.remove("show")
 }
 
 function handleAddSelect(){
-    dropdownmenu.classList.add("show");
+    dropdownmenu.classList.toggle("show");
     dropdowntoggle.classList.remove("selected");
-    handleAfterSelect();
+    nextbtn.setAttribute("disabled","disable")
+    // dropdowntoggle.addEventListener("blur",removeShow)
+    changeInnerText();
 }
 
 function init(){
     dropdowntoggle.addEventListener("click",handleAddSelect)
-    nextbtn.classList.add("disable")
+    // dropdowntoggle.addEventListener("blur",handleAddSelect)
 }
 
 init()
