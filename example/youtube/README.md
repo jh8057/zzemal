b5b515a34587b8f0d24e51a22bd740ab8243b230
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-07-31 02:37:11 +0900

    youtube
    Watched video & make better

---
+++ example/youtube/README.md
@@ -0,0 +1,4 @@
+position : sticky
+top : 0
+
+margin-bottom: calc(var(--padding) / 2);
--- example/youtube/youtube.js
+++ example/youtube/youtube.js
@@ -1,13 +1,27 @@
 "use strict";
 
+const titleContain = document.querySelector(".titleAndButton");
 const videotitle = document.querySelector(".videotitle");
+const moreBtn = titleContain.querySelector("button");
+const toolsicon = document.querySelector(".video-tools");
+const subscribe = document.querySelector(".subscribeicon");
+
+function handletoolsicon(e) {
+  console.log(e.target);
+}
 
 function handlemoretoggle() {
   videotitle.classList.toggle("more");
 }
 
+function handlesubscribe() {
+  subscribe.classList.toggle("subscribed");
+}
+
 function init() {
-  videotitle.addEventListener("click", handlemoretoggle);
+  moreBtn.addEventListener("click", handlemoretoggle);
+  subscribe.addEventListener("click", handlesubscribe);
+  // toolsicon.addEventListener("click", handletoolsicon);
 }
 
 init();
