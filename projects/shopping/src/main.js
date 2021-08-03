5bbd696fe46ca5c7ddc5051b5ca963d94b83e1a7
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-08-03 21:32:55 +0900

    shopping
    Make Css

--- projects/shopping/src/main.js
+++ projects/shopping/src/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const btn = document.querySelectorAll(".btn");
+
+function printlog(e) {
+  console.log(e);
+}
+
+function init() {
+  btn.forEach((btn) => btn.addEventListener("click", printlog));
+}
+init();
