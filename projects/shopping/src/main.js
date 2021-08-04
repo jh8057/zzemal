6dae06514d77ea598c5670844485babcbb33e6d3
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-08-04 15:31:15 +0900

    shopping
    Make JS about setEventListeners

--- projects/shopping/src/main.js
+++ projects/shopping/src/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const btn = document.querySelectorAll(".btn");
-// const itemsul = document.querySelector(".items");
 
 // Fetch the items from the JSON file
 function loadItems() {
@@ -26,12 +25,49 @@ function createHTMLString(item) {
   `;
 }
 
+// using fucniton displayItems()
+function onButtonClick(e, items) {
+  const key = e.target.dataset.key;
+  const value = e.target.dataset.value;
+  if (key == null || value == null) return;
+  let filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+// using classList
+function onButtonClick2(e) {
+  const key = e.target.dataset.key;
+  const value = e.target.dataset.value;
+  if (key == null || value == null) return;
+  UpdateItems(value);
+}
+
+// Check the value includes in innerHTML
+function UpdateItems(value) {
+  const items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    if (item.innerHTML.includes(value)) {
+      item.classList.remove("invisible");
+    } else {
+      item.classList.add("invisible");
+    }
+  });
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  // buttons.addEventListener("click", (e) => onButtonClick(e,items));
+  buttons.addEventListener("click", (e) => onButtonClick2(e));
+}
+
 // init function
 function init() {
   loadItems()
     .then((items) => {
       displayItems(items);
-      // setEventListeners(items);
+      setEventListeners(items);
     })
     .catch(console.log);
 }
