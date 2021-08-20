e87b4eaf829d81e9e18f6dc5806b3056d0c68bfa
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-08-21 00:33:10 +0900

    react_study
    Start react

---
+++ study/react_study/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
+import styles from "./App.module.css";
+function App() {
+  const name = "zzemal";
+  const mypage = {
+    name: "zzemal's github",
+    url: "https://github.com/jh8057/zzemal",
+  };
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "brown",
+          backgroundColor: "beige",
+        }}
+      >
+        Hello, {name}
+      </h1>
+      <a href={mypage.url} target="_blank" rel="noreferrer">
+        {mypage.name}
+      </a>
+      <Hello />
+      <Hello />
+      <Hello />
+      <div className={styles.box}>App</div>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
