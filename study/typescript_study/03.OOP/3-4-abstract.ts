63be21abf85b6663756bae75f36f065931ab127e
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-09 17:44:46 +0900

    typescript_study
    learn abstract

---
+++ study/typescript_study/03.OOP/3-4-abstract.ts
@@ -0,0 +1,60 @@
+{
+    type CoffeeCup = {
+        shots: number;
+        hasMilk:boolean;
+    }
+
+
+    interface CoffeMaker{
+        makeCoffee(shots:number):CoffeeCup;
+    }
+
+    // public
+    // private
+    // protected
+    class CoffeMachine implements CoffeMaker{
+        private static BEANS_GRAMM_PER_SHOT:number = 7 // class level
+        private coffeeBeans:number = 0; // instance(object) level
+
+        private constructor(coffeeBeans:number){
+            this.coffeeBeans = coffeeBeans;
+        }
+
+        static makeMachine(coffeeBeans:number): CoffeMaker{
+            return new CoffeMachine(coffeeBeans)
+        }
+
+        fillCoffeeBeans(beans:number){
+            if(beans < 0){
+                throw new Error('value for beans should be greater than 0');
+            }
+            this.coffeeBeans += beans;
+        }
+        private grindBeans(shots:number){
+            console.log(`grind ${shots}`)
+            if(this.coffeeBeans < shots * CoffeMachine.BEANS_GRAMM_PER_SHOT){
+                throw new Error("Not enough coffee");
+            }
+            this.coffeeBeans -= shots * CoffeMachine.BEANS_GRAMM_PER_SHOT;
+        }
+        private preheat():void{
+            console.log('heat')
+        }
+        private extract(shots:number):CoffeeCup{
+            console.log(`pulling ${shots}`)
+            return{
+                shots,
+                hasMilk : false,
+            }
+        }
+        makeCoffee(shots:number) :CoffeeCup {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots)
+        }
+
+    }
+    const maker:CoffeMaker = CoffeMachine.makeMachine(32);
+    maker.makeCoffee(2)
+    // maker.fillCoffeeBeans(32);
+}
