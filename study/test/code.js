203a5f89a5768b0b11de993124f73e211c9a4361
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-14 00:49:56 +0900

    test
    test API

--- study/test/code.js
+++ study/test/code.js
@@ -1,6 +1,28 @@
 const test1 = document.querySelector("span");
-const test2 = document.querySelector(".Wa");
+const answer = document.querySelector("answer");
 
 console.log(test1.dataset);
+const BASE_URL =
+  "https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
 
-test2.addEventListener("click", () => console.log("T"));
+//   x_auth_token: "386e92a781b7880d4890c623ad7f1c08",
+function getTokenApi() {
+  fetch(`${BASE_URL}/start`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      problem: 1,
+    }),
+  })
+    .catch(console.log)
+    .then((res) => {
+      if (res.ok) {
+        alert("start complete");
+        console.log(res.json());
+      }
+    });
+}
+// const parseToDos = JSON.parse(loadToDos);
+getTokenApi();
