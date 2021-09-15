8fd3f8551b40f820be4fbf9f3f3389f2510c3189
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-15 23:49:19 +0900

    typescript_study
    Learn OOP

--- study/typescript_study/04.OOP-quiz/stack.ts
+++ study/typescript_study/04.OOP-quiz/stack.ts
@@ -1,29 +1,49 @@
 interface stack {
   readonly size: number;
   pop(): string;
-  push(value: string): void;
+  push(value: string): string;
 }
 
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
 class stackMachine implements stack {
-  size = this.str.length;
-  constructor(private str: string) {}
-
-  pop(): string {
-    const target = this.str.substring(this.size - 1);
-    this.str = this.str.substring(0, this.size - 1);
-    this.size -= 1;
-    return target;
+  private _size: number = 0;
+  private head?: StackNode;
+  get size() {
+    return this._size;
   }
-
+  constructor(private capacity: number) {}
   push(value: string) {
-    this.str += value;
-    this.size += 1;
+    if (this.size === this.capacity) {
+      throw new Error("STACK IS FULL");
+    }
+    this._size++;
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
+    return `PUSH ${value}`;
+  }
+  pop(): string {
+    if (this.head == null) {
+      throw new Error("STACK IS EMPTY");
+    }
+    const node = this.head;
+    this.head = this.head.next;
+    this._size--;
+    return `POP ${node.value}`;
   }
 }
 
-let stackTest = new stackMachine("ABCD");
-console.log(stackTest);
-console.log(stackTest.pop());
-console.log(stackTest);
-console.log(stackTest.push("c"));
-console.log(stackTest);
+let stackTest = new stackMachine(10);
+// console.log(stackTest.size);
+stackTest.push("b");
+stackTest.push("c");
+stackTest.push("d");
+stackTest.push("d");
+
+while (stackTest.size !== 0) {
+  console.log(stackTest.pop());
+}
+
+// stackTest.pop();
