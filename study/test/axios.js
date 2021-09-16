82c00e6ee0227bb21cf526da7294d0198572904a
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-16 22:41:39 +0900

    test
    Make test API

--- study/test/axios.js
+++ study/test/axios.js
@@ -1,19 +1,103 @@
 const axios = require("axios");
+const BASE_URL =
+  "https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
+const TOKEN = "75c9aa74e9011c5ff1091c5fd783004b";
 
-async function printlog() {
-  let result = await axios({
-    method: "get",
-    url: "http://bit.ly/2mTM3nY",
-    responseType: "stream",
-  }).then((res) => {
-    console.log("FINISH");
-    return res;
-  });
-  //   console.log(result);
-  console.log(typeof result);
-  console.log(result.statusText);
+class KAKAOAPI {
+  startAPI() {
+    return axios({
+      method: "post",
+      url: `${BASE_URL}/start`,
+      headers: {
+        "X-Auth-Token": `${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        problem: 1,
+      },
+    }).then((res) => {
+      console.log("FINISH START");
+      return res.data.auth_key;
+    });
+  }
+  async locationAPI(startToken) {
+    return axios({
+      method: "get",
+      url: `${BASE_URL}/locations`,
+      headers: {
+        Authorization: `${startToken}`,
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log("FINISH LOCATION");
+      console.log(res.data);
+      return res.data;
+    });
+  }
+  async truckAPI(startToken) {
+    return axios({
+      method: "get",
+      url: `${BASE_URL}/trucks`,
+      headers: {
+        Authorization: `${startToken}`,
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log("FINISH TRUCK");
+      console.log(res.data);
+      return res.data;
+    });
+  }
+  async simulationAPI(startToken) {
+    return axios({
+      method: "put",
+      url: `${BASE_URL}/simulate`,
+      headers: {
+        Authorization: `${startToken}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        commands: [{ truck_id: 0, command: [2, 5, 2, 6] }],
+      },
+    }).then((res) => {
+      console.log("FINISH SIMULATION");
+      console.log(res.data);
+      return res.data;
+    });
+  }
+  async scoreAPI(startToken) {
+    return axios({
+      method: "get",
+      url: `${BASE_URL}/score`,
+      headers: {
+        Authorization: `${startToken}`,
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log("FINISH TRUCK");
+      console.log(res.data);
+      return res.data;
+    });
+  }
 }
-printlog();
 
-let test = [1, 2, 3, 4];
-console.log(test.pop());
+const problem1 = new KAKAOAPI();
+
+async function simulation() {
+  let sToken = await problem1.startAPI();
+  console.log(sToken);
+
+  let status = "ready";
+  while (status === "ready") {
+    let result = await problem1.simulationAPI(sToken);
+    status = result.status;
+  }
+
+  problem1.scoreAPI(sToken);
+}
+simulation();
+// problem1.simulationAPI();
+// problem1.simulationAPI();
+// problem1.simulationAPI();
+// problem1.simulationAPI();
+// problem1.locationAPI();
