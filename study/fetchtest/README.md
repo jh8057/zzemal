2e8e9d8e5b719525cc1da7520494c570cc71e663
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-17 18:27:04 +0900

    fetchtest
    Test fetch in node

---
+++ study/fetchtest/README.md
@@ -0,0 +1,4 @@
+1. npm init -> package.json 파일 생성
+2. package.json 파일에 "type" : "module" 추가
+3. npm install node-fetch
+4. `import fetch from "node-fetch";` 을 js파일에 삽입
---
+++ study/fetchtest/fetch.js
@@ -0,0 +1,12 @@
+// import fetch from "node-fetch";
+// const response = await fetch("https://github.com/");
+// const body = await response.text();
+
+// const json = '{"result":true, "count":42}';
+// const obj = JSON.parse(json);
+
+// console.log(obj.count);
+// // expected output: 42
+
+// console.log(obj.result);
+// // expected output: true
