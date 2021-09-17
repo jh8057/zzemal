ed6a07a5a4b57f3231dab86282ee4d5ceca04860
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-17 22:16:40 +0900

    typescript
    learn typescript generic

--- study/typescript_study/06.generic-quiz/stack-generic.ts
+++ study/typescript_study/06.generic-quiz/stack-generic.ts
@@ -1,41 +1,40 @@
-interface stack {
+interface stack<T> {
   readonly size: number;
-  pop(): string;
-  push(value: string): string;
+  push(value: T): void;
+  pop(): T;
 }
 
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
+type StackNode<T> = {
+  readonly value: T;
+  readonly next?: StackNode<T>;
 };
-class stackMachine implements stack {
+class stackMachine<T> implements stack<T> {
   private _size: number = 0;
-  private head?: StackNode;
+  private head?: StackNode<T>;
   get size() {
     return this._size;
   }
   constructor(private capacity: number) {}
-  push(value: string) {
+  push(value: T) {
     if (this.size === this.capacity) {
       throw new Error("STACK IS FULL");
     }
     this._size++;
-    const node: StackNode = { value, next: this.head };
+    const node = { value, next: this.head };
     this.head = node;
-    return `PUSH ${value}`;
   }
-  pop(): string {
+  pop(): T {
     if (this.head == null) {
       throw new Error("STACK IS EMPTY");
     }
     const node = this.head;
     this.head = this.head.next;
     this._size--;
-    return `POP ${node.value}`;
+    return node.value;
   }
 }
 
-let stackTest = new stackMachine(10);
+let stackTest = new stackMachine<string>(10);
 // console.log(stackTest.size);
 stackTest.push("b");
 stackTest.push("c");
@@ -47,3 +46,14 @@ while (stackTest.size !== 0) {
 }
 
 // stackTest.pop();
+
+let stackTest2 = new stackMachine<number>(10);
+// console.log(stackTest.size);
+stackTest2.push(123);
+stackTest2.push(456);
+stackTest2.push(678);
+stackTest2.push(789);
+
+while (stackTest2.size !== 0) {
+  console.log(stackTest2.pop());
+}
