bcab20162c12412b40415a93af6e39f670f96fba
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-09-28 20:10:52 +0900

    typescript_study
    Learn module

--- study/typescript_study/10-javascript/10-3-module1.js
+++ study/typescript_study/10-javascript/10-3-module1.js
@@ -1,3 +1,9 @@
-function add(a, b) {
+export default function add(a, b) {
   return a + b;
 }
+
+export function print() {
+  console.log("NOT DEFAULT");
+}
+
+export const testnum = 1;
--- study/typescript_study/10-javascript/10-3-module2.js
+++ study/typescript_study/10-javascript/10-3-module2.js
@@ -1,1 +1,11 @@
+import add, { print as printMessage, testnum } from "./10-3-module1.js";
+
+// 이렇게 전체를 불러올수도 있다.
+// calculator.print() 이런식으로 사용
+// 변수까지 가져올 수 있다.
+// import * as calculator from "./10-3-module1.js";
+
 console.log(add(1, 2));
+
+printMessage();
+console.log(testnum);
