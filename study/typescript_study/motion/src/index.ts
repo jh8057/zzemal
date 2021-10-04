6b87f87820e2ff117312df0a73197f91954655af
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-10-04 11:37:15 +0900

    motion
    test

--- study/typescript_study/motion/src/index.ts
+++ study/typescript_study/motion/src/index.ts
@@ -1,1 +1,10 @@
-console.log("main project ");
+const random = document.querySelector(".random__content");
+console.log("RANDOM", random);
+async function randomImage() {
+  const image = await fetch("https://picsum.photos/200");
+  console.log("image", image);
+  const result = document.createElement("img");
+  result.src = image.url;
+  random?.appendChild(result);
+}
+randomImage();
