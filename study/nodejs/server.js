782725b1f7cacf199e0ab7e1fe284d1a8a521a44
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-11-02 23:44:59 +0900

    node
    Study nodejs

--- study/nodejs/server.js
+++ study/nodejs/server.js
@@ -2,5 +2,21 @@ const http = require("http");
 http
   .createServer((request, response) => {
     console.log("server start!");
+    return request
+      .on("error", (err) => {
+        console.error(err);
+      })
+      .on("data", (data) => {
+        console.log(data);
+      })
+      .on("end", () => {
+        response.on("error", (err) => {
+          console.error(err);
+        });
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/plain");
+        response.write("hi\n");
+        response.end("the end!");
+      });
   })
   .listen(8080);
