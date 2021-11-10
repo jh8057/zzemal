e67b2788fe20439fca9639d5dff57fb46397b399
jh8057 <69341843+jh8057@users.noreply.github.com>
2021-11-11 00:11:21 +0900

    Nuxt
    Fixed node file

--- projects/NuxtNode/server/index.js
+++ projects/NuxtNode/server/index.js
@@ -6,19 +6,17 @@ app.use(routes);
 
 const config = require("../nuxt.config.js");
 const { Nuxt, Builder } = require("nuxt");
-config.dev = !(process.env.NODE_ENV === "production");
 const nuxt = new Nuxt(config);
+
+const http = require("http");
+const server = http.createServer(app);
+app.use(nuxt.render);
+
 if (config.dev) {
   const builder = new Builder(nuxt);
   builder.build();
 }
-app.use(nuxt.render);
 
-const http = require("http");
-const server = http.createServer(app);
-
-// Listen the server
 server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
-// Export express app
